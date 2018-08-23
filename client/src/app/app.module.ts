307dff852215f3c334eafc94a6c9d8c043290876
MrKSM <KSM>
2018-08-24 00:16:09 +1000

    Generated Menu, Login and Chat components for Client

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SocketService } from './services/socket.service';
+import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './login/login.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    LoginComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,

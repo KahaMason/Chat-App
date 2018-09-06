8faf6f2eb75454769eccf5825209fc1f1e685f99
MrKSM <KSM>
2018-09-06 21:26:27 +1000

    Added Login Functionality and Access Controls

--- client/src/app/chat/chat.component.ts
+++ client/src/app/chat/chat.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SocketService } from '../services/socket.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-chat',
@@ -7,31 +8,47 @@ import { SocketService } from '../services/socket.service';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
-  message: string;
-  messages: string[] = [];
+  message: string;  // Stores chat message
+  messages: string[] = []; // Stores collection of chat messages
+  username: string = sessionStorage.getItem('username');
   connection;
 
-  constructor(private socketService: SocketService) { }
+  constructor(private socketService: SocketService, private router: Router) { }
 
   ngOnInit() {
-    this.connection = this.socketService.getMessages().subscribe((message: string) => {
-      this.messages.push(message);
-      this.message = '';
-    });
+    // Checks if there is a stored username value from login and redirects to login page if not found.
+    if (!sessionStorage.getItem('username')) {
+      sessionStorage.clear();
+      alert('User has not logged In');
+      this.router.navigateByUrl('login');
+      
+    }
+    
+    // If Login confirmed, subsribe to socket.
+    else {
+      this.connection = this.socketService.getMessages().subscribe((message: string) => {
+        this.messages.push(message);
+        this.message = '';
+      });
+    }
   }
-
+  
+  // Sends message to all connected chat members over the server socket.
   sendMessage() {
-    this.socketService.sendMessage(this.message);
+    this.socketService.sendMessage('(' + this.username + '): ' + this.message);
   }
 
   ngOnDestroy() {
+    // Unsubscribe from socket when navigating away from chat page.
     if(this.connection) {
       this.connection.unsubscribe();
     }
   }
 
+  // Logout Function
   logout() {
     sessionStorage.clear();
     console.log("User has logged out - Session Cleared");
+    this.router.navigateByUrl('login');
   }
 }
--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username: string;
+  password: string;
 
-  constructor() { }
+  constructor(private router: Router, private form: FormsModule) { }
 
   ngOnInit() {
+    // Responds if User is already currently logged in. Redirect to Chat.
+    if (sessionStorage.getItem('username')) {
+      alert("User is already logged in as: " + sessionStorage.getItem('username') );
+    }
+  }
+
+  // Login Function when login form is submitted
+  userLogin (event) {
+    event.preventDefault();
+
+    sessionStorage.setItem("username", this.username);
+
+    this.router.navigateByUrl('chat');
   }
 
 }
